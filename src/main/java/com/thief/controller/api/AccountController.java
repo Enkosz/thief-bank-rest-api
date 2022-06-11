cab4f97ce88762e668d7be19075470ef311d7701
Zuppa <smn01@gmail.com>
2022-06-11 18:57:51 +0200

    fix form- encoded

--- src/main/java/com/thief/controller/api/AccountController.java
+++ src/main/java/com/thief/controller/api/AccountController.java
@@ -47,8 +47,18 @@ public class AccountController {
         return new ResponseEntity<>(account, headers, HttpStatus.OK);
     }
 
-    @PostMapping(consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    public AccountCreatedDto createAccount(@RequestBody CreateAccountDto createAccountDto) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE )
+    public AccountCreatedDto createAccount(@RequestBody @Valid CreateAccountDto createAccountDto) {
+        Account account = accountService.createAccount(createAccountDto);
+
+        AccountCreatedDto accountCreatedDto = new AccountCreatedDto();
+        accountCreatedDto.setId(account.getId());
+
+        return accountCreatedDto;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE )
+    public AccountCreatedDto createAccountFormEncoded(@Valid CreateAccountDto createAccountDto) {
         Account account = accountService.createAccount(createAccountDto);
 
         AccountCreatedDto accountCreatedDto = new AccountCreatedDto();
@@ -64,22 +74,43 @@ public class AccountController {
         return AccountMapper.fromDomainToAccountCompactDto(account);
     }
 
-    @PostMapping(value = "/{accountId}", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE )
     public AccountDepositDto addDeposit(@PathVariable String accountId, @RequestBody Map<String, String> values) {
         Double amount = Double.parseDouble(values.get("amount"));
 
         return accountService.deposit(accountId, amount);
     }
 
-    @PutMapping(value = "/{accountId}", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE )
+    public AccountDepositDto addDepositFormEncoded(@PathVariable String accountId, Map<String, String> values) {
+        Double amount = Double.parseDouble(values.get("amount"));
+
+        return accountService.deposit(accountId, amount);
+    }
+
+    @PutMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public AccountCompactDto updateAccount(@PathVariable String accountId, @RequestBody @Valid AccountUpdateDto accountUpdateDto) {
         Account account = accountService.updateAccount(accountId, accountUpdateDto.getName(), accountUpdateDto.getSurname());
 
         return AccountMapper.fromDomainToAccountCompactDto(account);
     }
 
-    @PatchMapping(value = "/{accountId}", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PutMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public AccountCompactDto updateAccountFormEncoded(@PathVariable String accountId, @Valid AccountUpdateDto accountUpdateDto) {
+        Account account = accountService.updateAccount(accountId, accountUpdateDto.getName(), accountUpdateDto.getSurname());
+
+        return AccountMapper.fromDomainToAccountCompactDto(account);
+    }
+
+    @PatchMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE )
     public AccountCompactDto patchAccount(@PathVariable String accountId, @RequestBody @Valid AccountPatchDto accountPatchDto) {
+        Account account = accountService.updateAccount(accountId, accountPatchDto.getName(), accountPatchDto.getSurname());
+
+        return AccountMapper.fromDomainToAccountCompactDto(account);
+    }
+
+    @PatchMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE )
+    public AccountCompactDto patchAccountFormEncoded(@PathVariable String accountId, @Valid AccountPatchDto accountPatchDto) {
         Account account = accountService.updateAccount(accountId, accountPatchDto.getName(), accountPatchDto.getSurname());
 
         return AccountMapper.fromDomainToAccountCompactDto(account);
